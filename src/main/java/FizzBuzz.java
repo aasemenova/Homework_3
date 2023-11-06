3b426d024871d568b32b4c12edecf2cc5061428e
Alena <oleshinets2018@yandex.ru>
2023-11-07 01:10:27 +0300

    Initial commit

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,11 +2,11 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                System.out.println("Число " + i + " - FizzBuzz");
             } else if (i % 3 == 0) {
-                System.out.println("fizz");
+                System.out.println("Число " + i + " - Fizz");
             } else if (i % 5 == 0) {
-                System.out.println("buzz");
+                System.out.println("Число " + i + " - Buzz");
             } else {
                 System.out.println("Число " + i + " не делится на 3 или на 5 без остатка");
             }
